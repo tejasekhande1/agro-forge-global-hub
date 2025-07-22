2569c71b10359c23b130b669d6e1af59e5259501
Tejas Ekhande <tejasekhande01@gmail.com>
2025-07-22 23:57:23 +0530

    Revert "Update navbar, homepage, and footer"
    This reverts commit f8cabc77bf03108bf6a85d9180e32c9266a26e04.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import About from "./pages/About";
-import Products from "./pages/Products";
 import Spices from "./pages/Spices";
-import Gallery from "./pages/Gallery";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -21,10 +18,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
           <Route path="/spices" element={<Spices />} />
-          <Route path="/gallery" element={<Gallery />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
