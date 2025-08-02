55d84f2dca00d848b9fff433e74b56d0f6b82895
Tejas Ekhande <tejasekhande01@gmail.com>
2025-08-02 16:56:36 +0530

    Integrate formspree

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -18,7 +18,7 @@ const Contact = () => {
         e.preventDefault();
         setLoading(true);
 
-        const response = await fetch("https://formspree.io/f/your_form_id", {
+        const response = await fetch("https://formspree.io/f/xjkoeyyn", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
